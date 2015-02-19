cfd0ca7e2ae78d2b5cd579038d067a2d3edf6b45
Brent Garner <bgarner@gmail.com>
2015-02-19 13:15:18 -0700

    added template and various migrations

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,23 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->command->info('User table seeded!');
 	}
 
+}
+
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('users')->delete();
+
+		User::create(array('username' => 'superadmin', 'first_name' => 'first1', 'last_name' => 'last1', 'role_id' => '1', 'store' => '0', 'email' => 'foo@bar.com', 'password' => Hash::make('1234')));
+		User::create(array('username' => 'storeadmin', 'first_name' => 'first2', 'last_name' => 'last2', 'role_id' => '2', 'store' => '123', 'email' => 'test@test.com', 'password' => Hash::make('1234')));
+		User::create(array('username' => 'sales1', 'first_name' => 'first3', 'last_name' => 'last3', 'role_id' => '3', 'store' => '123', 'email' => 'hello@hello.com', 'password' => Hash::make('1234')));
+		User::create(array('username' => 'sales2', 'first_name' => 'first4', 'last_name' => 'last4', 'role_id' => '3', 'store' => '123', 'email' => 'brent@brent.com', 'password' => Hash::make('1234')));
+		User::create(array('username' => 'sales3', 'first_name' => 'first5', 'last_name' => 'last5', 'role_id' => '4', 'store' => '123', 'email' => 'one@more.com', 'password' => Hash::make('1234')));
+	}
 }
--- app/models/User.php
+++ app/models/User.php
@@ -23,4 +23,107 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $hidden = array('password', 'remember_token');
 
+	/**
+	* Get the roles a user has
+	*/
+	public function roles()
+	{
+		return $this->belongsToMany('Role', 'users_roles');
+	}
+
+	/**
+	* Find out if User is an employee, based on if has any roles
+	*
+	* @return boolean
+	*/
+	public function isEmployee()
+	{
+		$roles = $this->roles->toArray();
+		return !empty($roles);
+	}
+
+	/**
+	* Find out if user has a specific role
+	*
+	* $return boolean
+	*/
+	public function can($check)
+	{
+		return in_array($check, array_fetch($this->roles->toArray(), 'name'));
+	}
+
+	/**
+	* Get key in array with corresponding value
+	*
+	* @return int
+	*/
+	private function getIdInArray($array, $term)
+	{
+		foreach ($array as $key => $value) {
+			if ($value == $term) {
+				return $key;
+			}
+		}
+
+		throw new UnexpectedValueException;
+	}
+
+	/**
+	*
+	*/
+	public function assignRole($title)
+	{
+		$assigned_roles = array();
+
+		//$roles = array_fetch(Role::all()->toArray(), 'name');
+		$roles_id = array_fetch(Role::all()->toArray(), 'id');
+		$roles_name = array_fetch(Role::all()->toArray(), 'name');
+
+		$roles = array_combine( $roles_id , $roles_name );
+
+		switch ($title) {
+			case 'super_admin':
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_store_admin');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_store_admin');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_store_admin');
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_staff');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_staff');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_staff');
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_order');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_order');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_order');
+				break;
+
+			case 'store_admin':
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_staff');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_staff');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_staff');
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_order');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_order');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_order');
+				break;
+
+			case 'staff':
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_customer');
+				$assigned_roles[] = $this->getIdInArray($roles, 'add_order');
+				$assigned_roles[] = $this->getIdInArray($roles, 'edit_order');
+				$assigned_roles[] = $this->getIdInArray($roles, 'delete_order');
+				break;
+
+			default:
+				throw new \Exception("The employee status entered does not exist");
+				break;
+		}
+
+		$this->roles()->attach($assigned_roles);
+	}
+
 }
